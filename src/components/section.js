e096796380849cfa973d0bb75ec8a0fa2471b7ca
Phoenix Mee <phoenixmee91@gmail.com>
2021-12-05 22:11:47 +0000

    Section first visual

--- src/components/section.js
+++ src/components/section.js
@@ -1,14 +1,21 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import useOnScreen from '../hooks/useOnScreen';
 import '../scss/section.scss';
 
 export default function Section(props) {
     const section = useRef();
     const title = useRef();
-    const isOnScreen = useOnScreen(section);
+    const [firstVisual, setFirstVisual] = useState(false);
+    let isOnScreen = useOnScreen(section);
+
+    useEffect(() => {
+        if (isOnScreen && !firstVisual) {
+            setFirstVisual(isOnScreen);
+        }
+    }, [isOnScreen]);
 
     return (
-        <div ref={section} className={'section' + (isOnScreen ? ' animate__push-left-fade-in' : '')}>
+        <div ref={section} className={'section' + (firstVisual ? ' animate__push-left-fade-in' : '')}>
             <h1 ref={title} className="animate--slow animate-right-border--horizontal-center">{props.section}</h1>
         </div>
     );
