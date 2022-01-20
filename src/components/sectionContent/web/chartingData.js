1b5f7b38c973105123db2e1a4df4427d53976ba6
Phoenix Mee <phoenixmee91@gmail.com>
2022-01-20 15:14:35 +0000

    CharingData: textual changes

--- src/components/sectionContent/web/chartingData.js
+++ src/components/sectionContent/web/chartingData.js
@@ -26,7 +26,7 @@ export default function ChartingData({type, title}) {
             <h1 className='title'>{title}</h1>
         </div>
         <div className='body'>
-            <p>There are many packages out there that tackle representing data in graphs, some more sophisticated and efficient than others. I'm a slight masochist, so let's create our own graphing canvas and try to round it off with click and hover events, while being as efficient as possible. Let's begin by creating a simple Line Chart, which will set us up for extending it into other charting types; also laying the foundations for the base class - the most important part of any component.</p>
+            <p>There are many packages out there that tackle representing data in graphs, some more sophisticated and efficient than others. I'm a slight masochist, so let's create our own graphing canvas and try to round it off with click and hover events, while being as efficient as possible. Let's begin by creating a simple LineChart class, which will set us up for extending it into other charting types; also laying the foundations for the base class - the most important part of any component.</p>
             <p><i>Code and explanation to follow soon.</i></p>
             <div ref={container} className='chart__container--medium chart__container--center'></div>
         </div>
